0f6879bce756ee37082a5ac4771f139118dd5178
Ruken Afşin <rukenafsin13@gmail.com>
2024-04-23 12:28:38 +0300

    add:weather.json

---
+++ weather_ex.json/weather.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://api.openweathermap.org/data/2.5/weather"
+access_key = ""
+
+response = requests.get(url, params={"lat": 41.0082, "lon": 28.9784, "appid": access_key})
+weather_data = response.json()
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+
+print("Şehir: ", weather_data["name"])
+print("Hava Durumu: ", weather_data["weather"][0]["description"])
+print("Sıcaklık: ", round(kelvin_to_celsius(weather_data["main"]["temp"]), 2), "°C")
+print("Hissedilen Sıcaklık: ", round(kelvin_to_celsius(weather_data["main"]["feels_like"]), 2), "°C")
+print("Minimum Sıcaklık: ", round(kelvin_to_celsius(weather_data["main"]["temp_min"]), 2), "°C")
+print("Maksimum Sıcaklık: ", round(kelvin_to_celsius(weather_data["main"]["temp_max"]), 2), "°C")
+print("Rüzgar Hızı: ", weather_data["wind"]["speed"], "m/s")
